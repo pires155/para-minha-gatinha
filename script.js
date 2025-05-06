664179fc22be83d53550a9292afa1d39a69f19e1
damasio <damasiodigo77@gmail.com>
2025-05-06 00:20:48 -0300

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function abrirEnvelope() {
     document.getElementById('envelope').classList.toggle('aberto');
   }
   
+  function criarCoracao() {
+    const coracoesContainer = document.querySelector('.coracoes');
+    const coracao = document.createElement('div');
+    coracao.classList.add('coracao');
+    coracao.style.left = Math.random() * 100 + 'vw';
+    coracao.style.animationDuration = 4 + Math.random() * 2 + 's';
+    coracoesContainer.appendChild(coracao);
+  
+    setTimeout(() => {
+      coracao.remove();
+    }, 6000);
+  }
+  
+  setInterval(criarCoracao, 300);
+
+  
+  
+  
