8a79ed541c647b7b19185c51bc10414308226652
damasio <damasiodigo77@gmail.com>
2025-05-06 00:59:42 -0300

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 function abrirEnvelope() {
-  document.getElementById('envelope').classList.toggle('aberto');
-}
-
-criarCoracoes(); // Chama os corações assim que a página abre
-
-
-// Função para criar os corações caindo
-function criarCoracoes() {
-  const container = document.querySelector('.coracoes-container');
-
-  setInterval(() => {
-    const coracao = document.createElement('div');
-    coracao.classList.add('coracao');
-
-    // Aleatoriza a posição, duração e tamanho
-    coracao.style.left = `${Math.random() * 100}vw`;
-    coracao.style.animationDuration = `${Math.random() * 3 + 3}s`;
-    coracao.style.width = coracao.style.height = `${Math.random() * 20 + 20}px`;
-
-    container.appendChild(coracao);
-
-    // Remove o coração depois de um tempo pra não pesar
-    setTimeout(() => {
-      coracao.remove();
-    }, 6000);
-  }, 300); // Cria um novo a cada 0.3s
-}
+    document.getElementById('envelope').classList.toggle('aberto');
+  }
+  
